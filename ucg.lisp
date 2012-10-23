e00ac5a10750f8a580aa0dac224e31a61ed9b8ab
Maximo Zholoback <ghostcheka@gmail.com>
2012-10-23 21:25:34 +0300

    Added escape symbol for raw lisp

--- ucg.lisp
+++ ucg.lisp
@@ -11,54 +11,34 @@
 (defgeneric print-in (grammar structure)
   (:documentation "Defines how to apply printing functions to structure."))
 
-(defmacro make-printer (name &key terminal default)
-  (let ((grammar (gensym)))
-    `(progn (defclass ,grammar nil
-	      ((built-in-funcs :initform (make-hash-table)
-			       :accessor built-in-funcs)
-	       (default-lambda :initarg :default
-		               :reader default-lambda)
-	       (terminal-lambda :initarg :terminal
-				:reader terminal-lambda)))
-	    (defmethod add-structure ((,grammar ,grammar)
-				      structure-name
-				      printing-lambda)
-	      (setf (gethash structure-name (built-in-funcs ,grammar))
-		    printing-lambda))
-	    (defmethod print-in ((,grammar ,grammar) structure)
-	      (when structure
-		(if (listp structure)
-		    (if (gethash (car structure)
-				 (built-in-funcs ,grammar))
-			(apply (gethash (car structure)
-					(built-in-funcs ,grammar))
-			       structure)
-			(if (fboundp (car structure))
-			    (print-in ,grammar
-				      (apply (car structure)
-					     (cdr structure)))
-			    (apply (default-lambda ,grammar) structure)))
-		    (funcall (terminal-lambda ,grammar) structure))))
-	    (defparameter ,grammar
-	      (make-instance ',grammar
-			     :terminal ,terminal
-			     :default ,default))
-	    (defun ,name (structure)
-	      (print-in ,grammar structure))
-	    (defun ,(intern (concatenate 'string
-					 "ADD-"
-					 (princ-to-string name)
-					 "-STRUCTURE"))
-		(structure-name printing-lambda)
-	      (add-structure ,grammar
-			     structure-name
-			     printing-lambda)))))
+(defmacro make-printer (name &key terminal default escape-symbol)
+  (let ((built-in-funcs (gensym)))
+    `(let ((,built-in-funcs (make-hash-table)))
+       (defun ,(intern (concatenate 'string
+				    "ADD-"
+				    (princ-to-string name)
+				    "-STRUCTURE"))
+	   (structure-name printing-lambda)
+	 (setf (gethash structure-name ,built-in-funcs)
+	       printing-lambda))
+       (defun ,name (structure)
+	 (when structure
+	   (if (listp structure)
+	       (if (eql ,escape-symbol (car structure))
+		   (funcall #',name
+			    (eval (second structure)))
+		   (if (gethash (car structure) ,built-in-funcs)
+			    (apply (gethash (car structure) ,built-in-funcs)
+				   structure)
+			    (apply ,default structure)))
+		    (funcall ,terminal structure)))))))
 
 ;; Example printer
 
 ;; here we define grammar and basic printing functions
 
 (make-printer html-grammar
+	      :escape-symbol 'raw-lisp
 	      :terminal (lambda (terminal) (princ-to-string terminal))
 	      :default (lambda (&rest unknown)
 			 (apply #'concatenate
@@ -79,6 +59,6 @@
 
 ;; and here we print some s-exp using `<name>' function
 
-(html-grammar '(:a (:b (:c "some text" (:d test)))))
+(html-grammar '(:a (:b (:c "some text" (raw-lisp (list :a " " (* 123 321)))))))
 
 ;; End of example
