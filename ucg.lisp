eb530f1ffbcc6273d87c7c1216f0c9b26910a175
Maximo Zholoback <ghostcheka@gmail.com>
2012-11-11 16:48:59 +0200

    Raw lisp deleted

--- ucg.lisp
+++ ucg.lisp
@@ -1,36 +1,33 @@
 (defpackage #:ucg
-  (:use #:cl))
+  (:use #:cl)
+  (:export #:make-printer))
 
 (in-package #:ucg)
 
-(defmacro make-printer (name &key terminal default escape-symbol)
+(defmacro make-printer (name &key terminal default)
   (let ((built-in-funcs (gensym)))
     `(let ((,built-in-funcs (make-hash-table)))
        (defun ,(intern (concatenate 'string
-				    "ADD-"
-				    (princ-to-string name)
-				    "-STRUCTURE"))
-	   (structure-name printing-lambda)
-	 (setf (gethash structure-name ,built-in-funcs)
-	       printing-lambda))
+                                    "ADD-"
+                                    (princ-to-string name)
+                                    "-STRUCTURE"))
+           (structure-name printing-lambda)
+         (setf (gethash structure-name ,built-in-funcs)
+               printing-lambda))
        (defun ,name (structure)
-	 (when structure
-	   (if (listp structure)
-	       (if (eql ,escape-symbol (car structure))
-		   (funcall #',name
-			    (eval (second structure)))
-		   (if (gethash (car structure) ,built-in-funcs)
-			    (apply (gethash (car structure) ,built-in-funcs)
-				   structure)
-			    (apply ,default structure)))
-		    (funcall ,terminal structure)))))))
+         (when structure
+           (if (listp structure)
+               (if (gethash (car structure) ,built-in-funcs)
+                   (apply (gethash (car structure) ,built-in-funcs)
+                          (cdr structure))
+                 (apply ,default structure))
+             (funcall ,terminal structure)))))))
 
 ;; Example printer
 
 ;; here we define grammar and basic printing functions
 
 (make-printer html-grammar
-	      :escape-symbol 'raw-lisp
 	      :terminal (lambda (terminal) (princ-to-string terminal))
 	      :default (lambda (&rest unknown)
 			 (apply #'concatenate
@@ -42,15 +39,15 @@
 
 (dolist (func-name (list :a :b :c))
   (add-html-grammar-structure func-name
-   (lambda (func &rest body)
+   (lambda (&rest body)
      (concatenate 'string
-		  "<" (princ-to-string func) ">"
+		  "<" (princ-to-string func-name) ">"
 		  (apply #'concatenate 'string
 			 (mapcar #'html-grammar body))
-		  "</" (princ-to-string func) ">"))))
+		  "</" (princ-to-string func-name) ">"))))
 
 ;; and here we print some s-exp using `<name>' function
 
-(html-grammar '(:a (:b (:c "some text" (raw-lisp (list :a " " (* 123 321)))))))
+(html-grammar `(:a (:b (:c "some text " ,(list :a (* 123 321))))))
 
 ;; End of example
