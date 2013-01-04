a7099c8b002059ab3bcab2588c20d31cbe6077bc
Zholobak Maxim <ghostcheka@gmail.com>
2013-01-04 21:33:18 +0200

    Some universalization

--- ucg.lisp
+++ ucg.lisp
@@ -5,23 +5,30 @@
 (in-package #:ucg)
 
 (defmacro make-printer (name &key terminal default)
-  (let ((built-in-funcs (gensym)))
-    `(let ((,built-in-funcs (make-hash-table)))
-       (defun ,(intern (concatenate 'string
-                                    "ADD-"
-                                    (princ-to-string name)
-                                    "-STRUCTURE"))
-           (structure-name printing-lambda)
-         (setf (gethash structure-name ,built-in-funcs)
-               printing-lambda))
+  (let ((funcs-and-macros (gensym)))
+    `(let ((,funcs-and-macros (make-hash-table)))
+       (defun ,(intern (format nil "~a-DEFUN" name))
+           (name lambda)
+         (setf (gethash name ,funcs-and-macros)
+               (cons :function lambda)))
+       (defun ,(intern (format nil "~a-DEFMACRO" name))
+           (name lambda)
+         (setf (gethash name ,funcs-and-macros)
+               (cons :macro lambda)))
        (defun ,name (structure)
          (when structure
            (if (listp structure)
-               (if (gethash (car structure) ,built-in-funcs)
-                   (apply (gethash (car structure) ,built-in-funcs)
-                          (cdr structure))
-                 (apply ,default structure))
-             (funcall ,terminal structure)))))))
+               (if (gethash (car structure) ,funcs-and-macros)
+		   (case (car (gethash (car structure) ,funcs-and-macros))
+		     (:function
+		      (apply (cdr (gethash (car structure) ,funcs-and-macros))
+			     (mapcar #',name (cdr structure))))
+		     (:macro
+		      (,name
+		       (apply (cdr (gethash (car structure) ,funcs-and-macros))
+			      (cdr structure)))))
+		   (apply ,default structure))
+	       (funcall ,terminal structure)))))))
 
 ;; Example printer
 
@@ -38,7 +45,7 @@
 ;; and here we add one specific function, using `add-<name>-structure'
 
 (dolist (func-name (list :a :b :c))
-  (add-html-grammar-structure func-name
+  (html-grammar-defun func-name
    (lambda (&rest body)
      (concatenate 'string
 		  "<" (princ-to-string func-name) ">"
@@ -46,8 +53,12 @@
 			 (mapcar #'html-grammar body))
 		  "</" (princ-to-string func-name) ">"))))
 
+(html-grammar-defmacro 'list
+		       (lambda (&rest args)
+			 args))
+
 ;; and here we print some s-exp using `<name>' function
 
-(html-grammar `(:a (:b (:c "some text " ,(list :a (* 123 321))))))
+(html-grammar `(:a (:b (:c "some text " (list :a ,(* 123 321))))))
 
 ;; End of example
